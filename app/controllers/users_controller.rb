336615f5ac882c1879456e5574d25d51f9a82a00
tosan777 <tosan.golang1.14.on@gmail.com>
2022-10-12 09:23:26 +0900

    ページング処理の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = User.where.not(id: current_user.id).order(:remember_created_at, :id)
+    @user = User.where.not(id: current_user.id).order(:remember_created_at, :id).page(params[:page]).per(3)
   end
 
   def show
