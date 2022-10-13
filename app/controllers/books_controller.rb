05c7d4c9c64e7a26ac8c88a87838a53d4ac6f856
tosan777 <tosan.golang1.14.on@gmail.com>
2022-10-13 09:25:25 +0900

    レビューに対しての修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: t('notice.create') }
+        format.html { redirect_to @book, notice: t('notice.created', name: "本") }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: t('notice.update') }
+        format.html { redirect_to @book, notice: t('notice.update', name: "本") }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('notice.destroy') }
+      format.html { redirect_to books_url, notice: t('notice.destroy', name: "本") }
       format.json { head :no_content }
     end
   end
