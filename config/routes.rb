0c288328f985e6f4aba6964c387fa381d9637ee7
tosan777 <tosan.golang1.14.on@gmail.com>
2022-11-08 11:44:17 +0900

    devise標準の翻訳を参照するため、viewのusersディレクトリをdeviseに名前を変更した。  Please enter the commit message for your changes. Lines starting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    passwords: 'users/passwords',
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
