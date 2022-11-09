723b38e9f836bd0f8fb63a05afcc062e6d086fcc
tosan777 <tosan.golang1.14.on@gmail.com>
2022-11-09 10:42:17 +0900

    ReportsモデルとCRUDアクションの生成

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,23 @@
+class ReportsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
