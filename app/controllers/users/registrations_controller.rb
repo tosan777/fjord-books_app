f5c94cbc53471643edcbbb72dd12cc1df8881d4f
tosan777 <tosan.golang1.14.on@gmail.com>
2022-11-10 09:12:53 +0900

    rubocopを通した。

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-
-  def create
-    super
-  end
-
-  def update
-    super
-  end
+  before_action :configure_sign_up_params, only: [:create] # rubocop:disable Rails/LexicallyScopedActionFilter
+  before_action :configure_account_update_params, only: [:update] # rubocop:disable Rails/LexicallyScopedActionFilter
 
   protected
 
