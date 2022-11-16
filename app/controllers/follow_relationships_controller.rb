4d18bcb8756d0c98051070a8d83cf778fca6ca1f
tosan777 <tosan.golang1.14.on@gmail.com>
2022-11-16 09:46:13 +0900

    フォロー、アンフォローした際のリダイレクト先を変更した。
    redirect.refererをリダイレクト先にしていたが脆弱性があるので、フォロー、アンフォローした対象のユーザーのインスタンス変数を用意して、そこにリダイレクトするようにした。

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -1,15 +1,23 @@
 # frozen_string_literal: true
 
 class FollowRelationshipsController < ApplicationController
+  before_action :other_user
+
   # フォローするとき
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    redirect_to @other_user
   end
 
   # フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    redirect_to @other_user
+  end
+
+  private
+
+  def other_user
+    @other_user = User.find(params[:user_id])
   end
 end
