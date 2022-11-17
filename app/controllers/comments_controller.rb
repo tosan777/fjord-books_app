94e8efee69d11f58758762c4d1a6b154860328f0
tosan777 <tosan.golang1.14.on@gmail.com>
2022-11-17 14:48:20 +0900

    コメントコントローラーのリファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,16 +4,14 @@ class CommentsController < ApplicationController
 
   def create
     if comment_params[:commentable_type].constantize.exists?(comment_params[:commentable_id])
-      @commentable = comment_params[:commentable_type].constantize.find(comment_params[:commentable_id])
+      @comment = Comment.new(comment_params)
+      @comment.user_id = current_user.id
+      @comment.save
+      flash[:success] = "コメントを投稿しました。"
+      redirect_to @comment.commentable
     else
       flash[:alert] = "この日報は存在しません。"
       return redirect_to users_path
-    end
-    @comment = @commentable.comments.new(comment_params)
-    @comment.user_id = current_user.id
-    if @comment.save
-      flash[:success] = "コメントを投稿しました。"
-      redirect_to @commentable
     end
   end
 
