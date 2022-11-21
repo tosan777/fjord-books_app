5baf26150d6986cd3b7b033e349415e36bffe8fe
tosan777 <tosan.golang1.14.on@gmail.com>
2022-11-21 11:48:37 +0900

    rubocopとlintを通した。

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class FollowRelationshipsController < ApplicationController
-
   # フォローするとき
   def create
     if User.exists?(id: params[:user_id])
@@ -9,7 +8,7 @@ class FollowRelationshipsController < ApplicationController
       current_user.follow(@other_user)
       redirect_to @other_user
     else
-      flash[:alert] = "このユーザーは存在しません。"
+      flash[:alert] = 'このユーザーは存在しません。'
       redirect_to users_path
     end
   end
@@ -21,9 +20,8 @@ class FollowRelationshipsController < ApplicationController
       current_user.unfollow(@other_user)
       redirect_to @other_user
     else
-      flash[:alert] = "このユーザーは存在しません。"
+      flash[:alert] = 'このユーザーは存在しません。'
       redirect_to users_path
     end
   end
-
 end
