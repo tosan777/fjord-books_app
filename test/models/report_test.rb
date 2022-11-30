c65528ac373ec4ed161fd11eb32b79866a64d1d7
tosan777 <tosan.golang1.14.on@gmail.com>
2022-11-30 14:41:54 +0900

    Reportクラスのモデルのテストを記述しました。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @one = reports(:one)
+    @two = reports(:two)
+    @yuji = users(:yuji)
+    @megumi = users(:megumi)
+  end
+
+  test 'editable?' do
+    assert @one.user == @yuji
+    assert_not @one.user == @megumi
+  end
+
+  test 'created_on' do
+    assert @one.created_at.to_date
+  end
 end
