0df7474fa094697c43e0e4e6ace232c32ca7e2c0
tosan777 <ksk.pleiades@gmail.com>
2022-12-08 09:46:19 +0900

    Reportモデルのテストの修正を行いました。

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -4,18 +4,18 @@ require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
   setup do
-    @one = reports(:one)
-    @two = reports(:two)
-    @yuji = users(:yuji)
+    @yuji_report   = reports(:yuji_report)
+    @megumi_report = reports(:megumi_report)
+    @yuji   = users(:yuji)
     @megumi = users(:megumi)
   end
 
   test 'editable?' do
-    assert @one.user == @yuji
-    assert_not @one.user == @megumi
+    assert_equal @yuji,   @yuji_report.user
+    assert_equal @megumi, @megumi_report.user
   end
 
   test 'created_on' do
-    assert @one.created_at.to_date
+    assert_equal 'Wed, 30 Nov 2022'.to_date, @yuji_report.created_at.to_date
   end
 end
